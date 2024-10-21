69f930903d250e4277096f94479e346463c3d2e0
Santiconquest <161180110+Santiconquest@users.noreply.github.com>
2024-10-21 14:51:58 +0000

    Se agregan endpoints para lector

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,28 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Lector(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    lastname = db.Column(db.String(120), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    suscription_date = db.Column(db.String(120), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+  
+
+
+    def __repr__(self):
+        return '<Usuario %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "lastname": self.lastname,
+            "suscription_date": self.suscription_date,
+            "active": self.is_active,
+            # do not serialize the password, its a security breach
+        }
