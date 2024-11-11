aa8d0b5d5d8f7bdba36780bb6e8762129c215e32
Santiconquest <161180110+Santiconquest@users.noreply.github.com>
2024-11-11 16:57:31 +0000

    Se agrega estilo a login admin

--- src/front/js/pages/loginAdmin.js
+++ src/front/js/pages/loginAdmin.js
@@ -17,30 +17,44 @@ const LoginAdmin = () => {
             {store.auth ? (
                 <Navigate to='/addbook' /> 
             ) : (
-                <form className="w-50 mx-auto" onSubmit={sendData}>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail" className="form-label">Email address</label>
-                        <input 
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            type="email"
-                            className="form-control"
-                            id="exampleInputEmail"
-                            aria-describedby="emailHelp"
-                        />
+                <div className="row marcolog">
+                    <div className="col-6">
+                            <h1 className="text-center">Log in Admin</h1>
+                        <form className="w-75 m-auto vh-100" onSubmit={sendData}>
+                            <div className="d-block">
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputEmail" className="form-label">Email address</label>
+                                <input 
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    type="email"
+                                    className="form-control"
+                                    id="exampleInputEmail"
+                                    aria-describedby="emailHelp"
+                                    placeholder="example@mail.com"
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputPassword" className="form-label">Password</label>
+                                <input
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    type="password"
+                                    className="form-control"
+                                    id="exampleInputPassword"
+                                    placeholder="********"
+                                />
+                            </div>
+                            <div>
+                                <button type="submit" className="w-100 btn btn-primary mt-4">Login</button>
+                            </div>
+                            </div>
+                        </form>
                     </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputPassword" className="form-label">Password</label>
-                        <input
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            type="password"
-                            className="form-control"
-                            id="exampleInputPassword"
-                        />
+                    <div className="col-6 right">
+                        <img className="w-100 vh-100" src="https://miwebenterrassa.com/wp-content/uploads/2024/11/blog-servidores.webp"/>
                     </div>
-                    <button type="submit" className="btn btn-primary">Login</button>
-                </form>
+                </div>
             )}
         </div>
     );
